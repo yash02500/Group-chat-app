3597fa5a1098d9673410019bcdb7c18928cb8635
yash02500 <yashv0482@gmail.com>
2024-07-19 19:04:45 +0530

    user signup frontend

---
+++ public/js/signup.js
@@ -0,0 +1,30 @@
+        
+async function addUser(event){
+    try{
+        event.preventDefault();
+
+        const userName = document.getElementById('name').value;
+        const userEmail = document.getElementById('email').value;
+        const userMob = document.getElementById('mobile').value;
+        const userPass = document.getElementById('password').value;
+
+        await axios.post('http://localhost:3000/user/signup', {
+            name: userName,
+            email: userEmail,
+            mobile: userMob,
+            password: userPass
+        });
+
+        window.location.href= "login.html";
+        console.log("User added");
+       
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('mobile').value = '';
+        document.getElementById('password').value = '';
+
+    } catch(error){
+        document.body.innerHTML=document.body.innerHTML+'<h4>Something Went Wrong</h4>';
+        console.log(error);
+    }
+}
